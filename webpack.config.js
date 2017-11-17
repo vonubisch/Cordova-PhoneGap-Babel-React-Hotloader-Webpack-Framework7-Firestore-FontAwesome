93dc980f3aa6ff06570576910c3b2ea331cda0d8
Bjorn Stefan von Ubisch <bs@vonubisch.com>
2017-11-17 12:20:43 +0100

    Remove old comments

--- webpack.config.js
+++ webpack.config.js
@@ -53,25 +53,16 @@ if (ENV === 'development') {
     module.exports = merge(common, {
         devServer: {
             contentBase: PATHS.build,
-
-            // Enable history API fallback so HTML5 History API based
-            // routing works. This is a good default that will come
-            // in handy in more complicated setups.
             historyApiFallback: true,
             hot: true,
             inline: true,
             progress: true,
-
-            // Display only errors to reduce the amount of output.
             stats: 'errors-only',
-
-            // Parse host and port from env so this is easy to customize.
             host: process.env.HOST,
             port: process.env.PORT
         },
         plugins: [new webpack.HotModuleReplacementPlugin()]
     });
 } else {
-    // config can be added here for minifying / etc
     module.exports = merge(common, {});
 }
