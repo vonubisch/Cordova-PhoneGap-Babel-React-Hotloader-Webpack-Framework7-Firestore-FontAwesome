5ff5f7282a510a91cbe1149cb52dfd5f8db07887
Bjorn Stefan von Ubisch <bs@vonubisch.com>
2017-11-19 12:26:47 +0100

    Make instance and router globally available

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,10 @@ import MainViews       from 'components/Main';
 import AppPopup        from 'components/Popup';
 import AppLoginScreen  from 'components/Login';
 
+let framework7, currentRoute = null;
+
+export const getFramework7 = () => framework7;
+export const getCurrentRoute = () => currentRoute;
 
 export class App extends React.Component {
 
@@ -35,7 +39,12 @@ export class App extends React.Component {
     render() {
         // console.log(this.props, this.state); // eslint-disable-line no-console
         return (
-            <Framework7App themeType="material" routes={routes}>
+            <Framework7App
+                onFramework7Init={f7 => framework7 = f7}
+                onRouteChange={route => currentRoute = route}
+                themeType="material"
+                routes={routes}
+            >
                 <Statusbar/>
                 {(this.state.loading) ? <Progressbar infinite color="blue" /> : null}
                 <LeftPanel/>
